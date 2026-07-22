1f54411294bcfaa0197279f534925d2da0c26e5c
efedemir282 <efedemir282@gmail.com>
2026-07-22 15:39:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,31 +13,37 @@ app.listen(PORT, () => {
 });
 
 function botuBaslat() {
-  console.log('Sunucuya bağlanılmaya çalışılıyor...');
+  console.log('Sunucuya bağlanılıyor...');
 
   const bot = mineflayer.createBot({
     host: 'play.reborncraft.pw',
     port: 25565,
     username: 'xBetray_31_AFK',
-    version: '1.16.5' // Sürüm sabitlendi
+    version: '1.16.5'
   });
 
   bot.on('spawn', () => {
-    console.log('Bot başarıyla oyuna girdi!');
+    console.log('Bot lobide doğdu!');
 
-    // 1. Şifre girişi (2. saniye)
+    // 1. ADIM: 3. saniyede Şifre Girer
     setTimeout(() => {
       bot.chat('/login efe43802');
-      console.log('Giriş şifresi gönderildi.');
-    }, 2000);
+      console.log('1. Şifre gönderildi.');
+    }, 3000);
 
-    // 2. Home noktasına ışınlanma (5. saniye)
+    // 2. ADIM: 7. saniyede Oyun Sunucusuna (Skyblock) Bağlanır
+    setTimeout(() => {
+      bot.chat('/skyblock'); // Sunucudaki mod ismi farklıysa (örn: /ada veya /is) burayı değiştirebilirsin
+      console.log('2. Skyblock sunucusuna geçiş komutu gönderildi.');
+    }, 7000);
+
+    // 3. ADIM: 12. saniyede Home Noktasına Işınlanır
     setTimeout(() => {
       bot.chat('/home');
-      console.log('Home noktasına ışınlanma komutu gönderildi.');
-    }, 5000);
+      console.log('3. Home noktasına ışınlanma komutu gönderildi.');
+    }, 12000);
 
-    // 3. AFK zıplama döngüsü (40 saniyede bir)
+    // 4. ADIM: 40 saniyede bir AFK kalmama zıplaması
     setInterval(() => {
       if (bot) {
         bot.setControlState('jump', true);
@@ -49,12 +55,11 @@ function botuBaslat() {
   });
 
   bot.on('kicked', (reason) => {
-    console.log('Bot sunucudan atıldı. Sebep:', reason);
+    console.log('Bot sunucudan atıldı:', reason);
   });
 
-  bot.on('end', (reason) => {
-    console.log('Bağlantı koptu. Detay/Sebep:', reason);
-    console.log('15 saniye sonra tekrar deneniyor...');
+  bot.on('end', () => {
+    console.log('Bağlantı koptu, 15 saniye sonra tekrar deneniyor...');
     setTimeout(botuBaslat, 15000);
   });
 
